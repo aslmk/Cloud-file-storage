eed7045725be9e6ce6b6894923b630a4059ffc83
aslmk <aulaensov@gmail.com>
2025-03-20 09:15:33 +0500

    feat(auth): implement user service and custom exception for registration

---
+++ src/main/java/com/aslmk/cloudfilestorage/service/Impl/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.aslmk.cloudfilestorage.service.Impl;
+
+import com.aslmk.cloudfilestorage.dto.RegisterDto;
+import com.aslmk.cloudfilestorage.entity.UserEntity;
+import com.aslmk.cloudfilestorage.repository.UserRepository;
+import com.aslmk.cloudfilestorage.service.UserService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void save(RegisterDto user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(user.getUsername());
+        userEntity.setPassword(passwordEncoder.encode(user.getPassword()));
+        userEntity.setEnabled(true);
+        userRepository.save(userEntity);
+    }
+
+    @Override
+    public Optional<UserEntity> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("User '%s' not found", username)
+        ));
+
+        return User.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .roles("USER")
+                .disabled(!userEntity.getEnabled())
+                .build();
+    }
+}
---
+++ src/main/java/com/aslmk/cloudfilestorage/service/UserService.java
@@ -0,0 +1,14 @@
+package com.aslmk.cloudfilestorage.service;
+
+import com.aslmk.cloudfilestorage.dto.RegisterDto;
+import com.aslmk.cloudfilestorage.entity.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
+
+public interface UserService extends UserDetailsService {
+
+    void save(RegisterDto user);
+
+    Optional<UserEntity> findByUsername(String username);
+}
