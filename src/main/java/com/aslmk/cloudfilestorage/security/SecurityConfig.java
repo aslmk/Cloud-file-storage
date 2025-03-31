617a386642f253e1f1bfe79c2cb8aad73be63e29
aslmk <aulaensov@gmail.com>
2025-03-31 10:05:37 +0500

    feat(security): enable csrf.

--- src/main/java/com/aslmk/cloudfilestorage/security/SecurityConfig.java
+++ src/main/java/com/aslmk/cloudfilestorage/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,11 +31,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         request -> request
-                        .requestMatchers("/auth/*").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers("/auth/*").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/auth/login")
