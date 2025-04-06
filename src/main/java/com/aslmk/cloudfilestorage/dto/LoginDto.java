4dcbac981aa8d3062809e3fce4d52943cca82d30
aslmk <aulaensov@gmail.com>
2025-04-06 23:01:20 +0500

    feat: add validation to fields of LoginDto and RegisterDto

--- src/main/java/com/aslmk/cloudfilestorage/dto/LoginDto.java
+++ src/main/java/com/aslmk/cloudfilestorage/dto/LoginDto.java
@@ -1,11 +1,16 @@
 package com.aslmk.cloudfilestorage.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must contain at least from 3 to 20 symbols")
     private String username;
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 }
