032016172234e01701252d2c2eef6b81f279affe
aslmk <aulaensov@gmail.com>
2025-06-26 19:01:54 +0500

    refactor: update path input handling in user-file.html and user-folder.html
    - simplify getParameterByName() method in urlParams.js
    - change input IDs in user-file.html and user-folder.html from "path" to "path-file" and "path-folder"
    - update urlParams.js to use the new input IDs

--- src/main/resources/static/js/urlParams.js
+++ src/main/resources/static/js/urlParams.js
@@ -1,15 +1,13 @@
 function getParameterByName(name) {
-    const url = window.location.href;
-    name = name.replace(/[$$$$]/g, '\\$&');
-    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-    const results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return results[2];
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const pathValue = getParameterByName('path');
-    document.getElementById('path').value = pathValue;
+
+    document.getElementById('path-file').value = pathValue;
+    document.getElementById('path-folder').value = pathValue;
     document.getElementById('renamePath').value = pathValue;
 });
