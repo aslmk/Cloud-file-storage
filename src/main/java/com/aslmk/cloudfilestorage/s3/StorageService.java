c9bcda65eba807c9cdc990f9afa730c258ce9965
aslmk <aulaensov@gmail.com>
2025-07-14 11:46:52 +0500

    refactor: remove StorageService with MinioServiceImpl
    - StorageService was removed because it was replaced by FileService and FolderService

--- src/main/java/com/aslmk/cloudfilestorage/s3/StorageService.java
+++
@@ -1,9 +0,0 @@
-package com.aslmk.cloudfilestorage.s3;
-
-import com.aslmk.cloudfilestorage.dto.UploadItemRequestDto;
-
-public interface StorageService {
-    void saveItem(UploadItemRequestDto item);
-    void renameItem(String oldItemName, String newItemName);
-    void removeItem(String itemName);
-}
